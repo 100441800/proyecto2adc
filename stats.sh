d9885dc6a301ca1e3887996de2c96efd62d4952e
José David Rico Días <davidricodias.dev@gmail.com>
2022-11-16 18:16:51 +0100

    Separate stats

--- stats.sh
+++ stats.sh
@@ -4,14 +4,20 @@
 
 export LD_LIBRARY_PATH=/opt/gcc-12.1.0/lib64 
 echo "AOS MONO" 
-perf stat -e power/energy-cores/,power/energy-pkg/,power/energy-ram/ -a -d release/img-aos in out mono  
+perf stat -a -d release/img-aos in out mono
+perf stat -e power/energy-cores/,power/energy-pkg/,power/energy-ram/ release/img-aos in out mono  
 echo "AOS HISTO" 
+perf stat -a -d release/img-aos in out histo
 perf stat -e power/energy-cores/,power/energy-pkg/,power/energy-ram/ -a -d release/img-aos in out histo  
 echo "AOS GAUSS" 
+perf stat -a -d release/img-aos in out gauss
 perf stat -e power/energy-cores/,power/energy-pkg/,power/energy-ram/ -a -d release/img-aos in out gauss  
 echo "SOA MONO" 
+perf stat -a -d release/img-soa in out mono
 perf stat -e power/energy-cores/,power/energy-pkg/,power/energy-ram/ -a -d release/img-soa in out mono  
 echo "SOA HISTO" 
+perf stat -a -d release/img-soa in out histo
 perf stat -e power/energy-cores/,power/energy-pkg/,power/energy-ram/ -a -d release/img-soa in out histo  
 echo "SOA GAUSS" 
+perf stat -a -d release/img-soa in out gauss
 perf stat -e power/energy-cores/,power/energy-pkg/,power/energy-ram/ -a -d release/img-soa in out gauss 
